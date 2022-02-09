9333959264fad5401ee0cd8905576b7f9743cb4d
thijs_abc <thijsalexander@outlook.com>
2022-02-09 13:24:07 +0100

    change plot and add some colors and layout

--- vb.py
+++ vb.py
@@ -1,11 +1,20 @@
 import matplotlib.pyplot as plt
 
+
 x = range(0,10)
 y = range(10, 20)
 
+
+#plotting
 plt.figure()
 
-plt.plot(x, y)
+plt.scatter(x, y, c='k')
+
+
+# opmaak
+plt.xlabel("$x$", c='violet')
+plt.ylabel("$y$", c='green')
+okt.grid()
 
 plt.show()
 
